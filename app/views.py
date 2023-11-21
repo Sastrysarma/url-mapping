9e44885701b3b76baca837724c936a3d542ffa75
Sastrysarma <naraseswarakotra1@gmail.com>
2023-11-21 12:29:47 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def srujana(request):
+    return render(request,'srujana.html')
+def shiva(request):
+    return render(request,'shiva.html')
